4ff578854a53009cf1e2e0f9d391ce7b630939f6
Руслан Чекушко <chekushkor@gmail.com>
2021-07-21 15:39:49 +0300

    --

---
+++ 02. Раздел/01.js
@@ -0,0 +1,15 @@
+// // console.log("ddd")
+// // console.log('ddddd')
+
+// 4 ? console.log('true') : console.log('false')
+
+var personAge = 20
+var message
+
+if (personAge < 18) {
+    message = 'Человек не совершеннолетний'    
+} else {
+    message = 'Человек совершеннолетний'
+}
+
+console.log(message)
